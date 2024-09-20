a85e24469bad259fa71b23f901206087f3941f8b
dina <dinaelhadidy966@gmail.com>
2024-09-20 14:10:48 +0300

    Redefine page structure in case of small screen

--- src/demo/Demo.js
+++ src/demo/Demo.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./Nav";
 import Hero from "./Hero";
 import Slider from "./Slider";
 
 import "./Demo.css";
 import Voucher from "./Voucher";
+import useIsSmallScreen from "../hooks/useIsSmallScreen";
 
 function Demo() {
+  const isSmallScreen = useIsSmallScreen();
+  console.log(isSmallScreen);
   return (
-    <div className="parent">
-      <div className="left-section">
-        <Nav />
-        <Hero />
-        <Voucher />
-      </div>
-      <div className="right-section">
-        <Slider />
-      </div>
-    </div>
+    <>
+      {!isSmallScreen ? (
+        <div className="parent">
+          <div className="left-section">
+            <Nav />
+            <Hero />
+            <Voucher />
+          </div>
+          <div className="right-section">
+            <Slider />
+          </div>
+        </div>
+      ) : (
+        <div className="parent">
+          <div className="left-section">
+            <Nav />
+            <Hero />
+            <Slider />
+            <Voucher />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
